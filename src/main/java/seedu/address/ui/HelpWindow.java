dfcf940c69a83c7a9433ceb7be4fa88d3516ac61
keezhenxian <zhenxian707@gmail.com>
2025-10-21 15:14:02 +0800

    Only 1 default contacts

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -10,7 +10,6 @@ import com.google.i18n.phonenumbers.PhoneNumberUtil;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
