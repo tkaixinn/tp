c501971728edd308195c9fa9359c6387b930e64b
keezhenxian <zhenxian707@gmail.com>
2025-10-21 15:45:50 +0800

    Debugged default data

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -28,7 +28,8 @@ public class SampleDataUtil {
         return new Person[]{
             new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
                     new Address("Blk 30 Geylang Street 29, #06-40"), new Country("Singapore"),
-                    new Culture("follows Chinese Traditions"), getTagSet("friends"))
+                    new Culture("follows Chinese Traditions"), Person.CommunicationChannel.EMAIL,
+                    getTagSet("friends"))
         };
     }
 
